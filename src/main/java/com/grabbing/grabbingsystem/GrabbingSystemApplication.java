76284688f0fdd769e7e941a905510fa978e65229
xingguanglang <826795780@qq.com>
2026-02-20 12:28:22 -0800

    first upload

---
+++ src/main/java/com/grabbing/grabbingsystem/GrabbingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.grabbing.grabbingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabbingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabbingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grabbing/grabbingsystem/GrabbingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabbing.grabbingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabbingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
